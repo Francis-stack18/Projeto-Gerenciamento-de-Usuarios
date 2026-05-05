f4bb96159b2a3fb4e38f5d5e3cc9a0a4d5a035ba
Francis <francis13322@gmail.com>
2026-05-05 18:30:23 -0300

    G13 - Trabalhando com arquivos - FileReader

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -8,8 +8,25 @@ class UserController {
   onSubmit() {
     this.formEl.addEventListener("submit", event => {
         event.preventDefault();
-        this.addLine(this.getValues())
+        let values = this.getValues()
+            this.getPhoto((content)=>{
+                values.photo = content
+                this.addLine(values)
+            })
       });
+  }
+
+  getPhoto(callback){
+    let fileReader = new FileReader();
+    let elements = [...this.formEl.elements].filter(item =>{
+        if(item.name === "photo"){
+            return item
+        }
+    })
+    fileReader.onload =() =>{
+        callback(fileReader.result)
+    }
+    fileReader.readAsDataURL()
   }
 
   getValues() {
@@ -40,7 +57,7 @@ class UserController {
     addLine(dataUser) {
     this.tableEl.innerHTML = `
         <tr>
-            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td><img src="${}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${dataUser.admin}</td>
