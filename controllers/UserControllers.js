b305f12433ec8748a1a78bbad59a7a8d92e4a0db
Francis <francis13322@gmail.com>
2026-05-05 23:00:38 -0300

    G18 - Validando Formulários

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -16,7 +16,7 @@ class UserController {
           values.photo = content;
           this.addLine(values);
 
-          this.formEl.reset()
+          this.formEl.reset();
           btn.disabled = false;
         },
         (e) => {
@@ -51,8 +51,16 @@ class UserController {
 
   getValues() {
     let user = {};
-
+    let isValid = true;
     [...this.formEl.elements].forEach(function (field, index) {
+      if (
+        ["name", "email", "password"].indexOf(field.name) > -1 &&
+        !field.value
+      ) {
+        field.parentElement.classList.add("has-error");
+        isValid = false;
+      }
+
       if (field.name == "gender") {
         if (field.checked) {
           user[field.name] = field.value;
@@ -63,6 +71,10 @@ class UserController {
         user[field.name] = field.value;
       }
     });
+
+    if (!isValid) {
+      return false;
+    }
 
     return new User(
       user.name,
