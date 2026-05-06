a5a0cfea462f02af5757d783ad1668d5ec46de3c
Francis <francis13322@gmail.com>
2026-05-05 22:19:58 -0300

    G15 - Ajustada a viw do projeto e operador ternário

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -2,39 +2,46 @@ class UserController {
   constructor(formId, tableId) {
     this.formEl = document.getElementById(formId);
     this.tableEl = document.getElementById(tableId);
-    this.onSubmit()
+    this.onSubmit();
   }
 
   onSubmit() {
-    this.formEl.addEventListener("submit", event => {
-        event.preventDefault();
-        let values = this.getValues()
-        this.getPhoto().then((content) => {
-            values.photo = content
-                this.addLine(values)
-        },(e)=>{
-            console.error(e)
-        })
-      });
+    this.formEl.addEventListener("submit", (event) => {
+      event.preventDefault();
+      let values = this.getValues();
+      this.getPhoto().then(
+        (content) => {
+          values.photo = content;
+          this.addLine(values);
+        },
+        (e) => {
+          console.error(e);
+        },
+      );
+    });
   }
 
-  getPhoto(){
+  getPhoto() {
     return new Promise((resolve, reject) => {
-    let fileReader = new FileReader();
-    let elements = [...this.formEl.elements].filter(item =>{
-        if(item.name === "photo"){
-            return item
+      let fileReader = new FileReader();
+      let elements = [...this.formEl.elements].filter((item) => {
+        if (item.name === "photo") {
+          return item;
         }
-    })
-    let file = elements[0].files[0]
-    fileReader.onload =() =>{
-        resolve(fileReader.result)
-    }
-    fileReader.onerror = (e)=>{
-        reject(e)
-    }
-    fileReader.readAsDataURL(file)
-    })
+      });
+      let file = elements[0].files[0];
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+      fileReader.onerror = (e) => {
+        reject(e);
+      };
+      if (file) {
+        fileReader.readAsDataURL(file);
+      } else {
+        resolve("dist/img/boxed-bg.jpg");
+      }
+    });
   }
 
   getValues() {
@@ -45,6 +52,8 @@ class UserController {
         if (field.checked) {
           user[field.name] = field.value;
         }
+      } else if (field.name == "admin") {
+        user[field.name] = field.checked;
       } else {
         user[field.name] = field.value;
       }
@@ -62,19 +71,19 @@ class UserController {
     );
   }
 
-    addLine(dataUser) {
-    this.tableEl.innerHTML = `
-        <tr>
+  addLine(dataUser) {
+    let tr = document.createElement("tr");
+    tr.innerHTML = `
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
-            <td>${dataUser.admin}</td>
+            <td>${(dataUser.admin) ? "Sim" : "Não"}</td>
             <td>${dataUser.birth}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
-        </tr>
     `;
-}
+    this.tableEl.appendChild(tr);
+  }
 }
